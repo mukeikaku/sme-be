bdf27ba149b96a59acbde969ac602f892c01b75c
ryota-mukeikaku <moriyama_ryota@mukeikaku.co.jp>
2023-02-06 19:01:44 +0900

    Modify deploy scripts: :

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-find /var/www/html/wp-content/themes -type d -exec chmod 705 {} \;
-find /var/www/html/wp-content/themes -type f -exec chmod 604 {} \;
+find /var/www/html/wp-content/themes -type d -exec chmod 755 {} \;
+find /var/www/html/wp-content/themes -type f -exec chmod 644 {} \;
 chown -R ec2-user:apache /var/www/html/wp-content/themes
 
-find /var/www/html/wp-content/plugins -type d -exec chmod 705 {} \;
-find /var/www/html/wp-content/plugins -type f -exec chmod 604 {} \;
+find /var/www/html/wp-content/plugins -type d -exec chmod 755 {} \;
+find /var/www/html/wp-content/plugins -type f -exec chmod 644 {} \;
 chmod -R 777 /var/www/html/wp-content/plugins/siteguard/really-simple-captcha/tmp
 chown -R ec2-user:apache /var/www/html/wp-content/plugins
