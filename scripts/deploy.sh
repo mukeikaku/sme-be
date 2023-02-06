d8b3ed1e4b7bd07a0f3b1ddec5c79c2e2894c4d8
ryota-mukeikaku <moriyama_ryota@mukeikaku.co.jp>
2023-02-06 18:44:22 +0900

    Modify appspec

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+find /var/www/html/wp-content/themes -type d -exec chmod 705 {} \;
+find /var/www/html/wp-content/themes -type f -exec chmod 604 {} \;
+chown -R ec2-user:apache /var/www/html/w-content/themes
+
+find /var/www/html/wp-content/plugins -type d -exec chmod 705 {} \;
+find /var/www/html/wp-content/plugins -type f -exec chmod 604 {} \;
+chmod -R 777 /var/www/html/wp-content/plugins/siteguard/really-simple-captcha/tmp
+chown -R ec2-user:apache /var/www/html/w-content/plugins
